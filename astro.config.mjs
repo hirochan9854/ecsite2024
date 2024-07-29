8cf3d0dc814dfffa993506de7b3c885276485240
hirochan9854 <ito.hiro9854@gmail.com>
2024-07-29 10:01:48 +0900

    画像の入れ込み

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import preact from "@astrojs/preact";
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [preact(), tailwind()],
+
+  // サイトのベースURLを設定
+  base: "/03_ecsite",
+
+  // ビルド出力ディレクトリを設定（デフォルトは 'dist'）
+  outDir: "./dist/03_ecsite",
 });
